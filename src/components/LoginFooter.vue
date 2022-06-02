5be70b86e515dbe96d482f1013d6814a2d5d2fcb
Vitor Duggen <zduggem@gmail.com>
2022-06-02 15:44:18 -0300

    Criado novos componentes do login/register

---
+++ src/components/LoginFooter.vue
@@ -0,0 +1,70 @@
+<template>
+    <footer class="footer">
+        <div class="footer__help" v-if="isLogin">
+            <v-checkbox
+                hide-details
+                dark
+                label="Remember me"
+                class="footer__remember"
+            />
+
+            <a href="#" class="footer__forget">Forgot password?</a>
+        </div>
+
+        <v-btn
+            color="#20DF7F"
+            large
+            block
+            dark
+            class="footer__button"
+        >
+            {{ getText }}
+        </v-btn>
+    </footer>
+</template>
+
+<script>
+    export default {
+        props: {
+            isLogin: {
+                type: Boolean,
+                default: false
+            }
+        },
+
+        computed: {
+            getText() {
+                return this.isLogin ? "Login" : "Create account"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+.footer {
+    $ref: &;
+
+    &__help {
+        @include d-flex(center, space-between);
+        margin-bottom: get-spacer(8);
+    
+        #{$ref}__remember {
+            margin-top: 0;
+            padding-top: 0;
+            @include font-family;
+        }
+
+        #{$ref}__forget {
+            @include link;
+            text-align: right;
+        }
+    }
+
+    &__button {
+        text-transform: unset;
+        @include border-radius;
+        font: 400 .875rem $font-family; 
+    }
+}
+</style>
---
+++ src/components/LoginHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class="header">
+        <span class="header__title">
+            {{ getText }}
+        </span>
+
+        <span class="header__subtitle">
+            Lorem ipsum dolor sit amet consectetur!
+        </span>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        isLogin: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        getText() {
+            return this.isLogin ? "Login" : "Sign in";
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+.header {
+    @include flex-break;
+
+    &__title {
+        line-height: normal;
+        font: 400 4rem $font-family;
+    }
+
+    &__subtitle {
+        margin: get-spacer(9) 0;
+        line-height: normal;
+        font: 400 1rem $font-family;
+    }
+}
+</style>
