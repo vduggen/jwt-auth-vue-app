43a48329b03ca7840d2e6b7061c336929e00b2c3
Vitor Duggen <zduggem@gmail.com>
2022-06-02 16:17:25 -0300

    Criado componentes bases

---
+++ src/components/Base/BaseField.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-text-field
+        solo
+        dark
+        background-color="#3c5e6a"
+        class="input"
+        v-bind="$attrs"
+        v-on="$listeners"
+    />
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+
+.input {
+    @include border-radius;
+}
+</style>
---
+++ src/components/Base/BaseFieldPassword.vue
@@ -0,0 +1,34 @@
+<template>
+    <BaseField
+        v-bind="$attrs"
+        label="Password"
+        :append-icon="input.icon"
+        :type="input.type"
+        @click:append="viewPassword = !viewPassword"
+    />
+</template>
+
+<script>
+import BaseField from "./BaseField";
+
+export default {
+    components: {
+        BaseField
+    },
+
+    data() {
+        return {
+            viewPassword: false
+        };
+    },
+
+    computed: {
+        input() {
+            return {
+                icon: this.viewPassword ? "mdi-eye" : "mdi-eye-off",
+                type: this.viewPassword ? "text" : "password",
+            };
+        }
+    },
+}
+</script>
