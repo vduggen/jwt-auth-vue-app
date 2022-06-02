54c505db4a3c990a333a1958983f18a67ee04a82
Vitor Duggen <zduggem@gmail.com>
2022-06-02 16:41:18 -0300

    Criado componente BaseBackground

---
+++ src/components/Base/BaseBackground.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="background">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/main.scss";
+.background {
+    $ref: &;
+
+    background-color: $secondary;
+    background-image: url('../../assets/Vectors.svg');
+    background-size: contain;
+    background-position-y: bottom;
+    color: $white;
+
+    @include set-w-h(100%);
+    @include d-flex(center, center, column);
+}
+</style>
