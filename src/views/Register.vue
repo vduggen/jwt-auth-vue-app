52be128263896a6f6de4b2c1b553ee60585f1bd9
Vitor Duggen <zduggem@gmail.com>
2022-06-06 17:04:17 -0300

    Feito lógica da função

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,19 @@
 <template>
-    <LoginWrapper>
-        <BaseField label="Name" />
+    <LoginWrapper
+        :loading="loading"
+        @action-button="handleRegister"
+    >
+        <v-form
+            ref="formRegister"
+            v-model="valid"
+            lazy-validation
+        >
+            <BaseField label="Name" />
 
-        <BaseField label="Login" />
+            <BaseField label="Login" />
 
-        <BaseFieldPassword />
+            <BaseFieldPassword />
+        </v-form>
     </LoginWrapper>
 </template>
 
@@ -12,12 +21,52 @@
 import LoginWrapper from '@/components/Login/LoginWrapper.vue';
 import BaseField from '@/components/Base/BaseField.vue';
 import BaseFieldPassword from '@/components/Base/BaseFieldPassword.vue';
+import { mapGetters } from 'vuex';
+import User from '../models/user';
 
 export default {
     components: {
         LoginWrapper,
         BaseField,
         BaseFieldPassword
+    },
+
+    computed: {
+        ...mapGetters({
+            getLoggedIn: 'auth/getLoggedIn'
+        })
+    },
+
+    data() {
+        return {
+            loading: false,
+            valid: true,
+            credentials: new User()
+        }
+    },
+
+    mounted() {
+        if (this.getLoggedIn) {
+            this.$router.push('/');
+        }
+    },
+
+    methods: {
+        async handleRegister() {
+            this.loading = true;
+
+            if (this.$refs.formRegister.validate()) {
+                try {
+                    await this.$store.dispatch('auth/register', this.credentials);
+
+                    this.$router.push('/');
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            this.loading = false;
+        }
     }
 }
 </script>
